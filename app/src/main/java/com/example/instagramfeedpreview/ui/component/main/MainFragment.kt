cc87c268b98149d6181907097bddaabe4a28038c
YuYangWoo <yuyw0712@naver.com>
2023-03-21 15:33:39 +0900

    [#10] refactor: 클린아키텍처 구조로 변경

--- app/src/main/java/com/example/instagramfeedpreview/ui/component/main/MainFragment.kt
+++ app/src/main/java/com/example/instagramfeedpreview/ui/component/main/MainFragment.kt
@@ -1,6 +1,8 @@
 package com.example.instagramfeedpreview.ui.component.main
 
 import android.graphics.Color
+import android.util.Log
+import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -8,7 +10,9 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.instagramfeedpreview.R
 import com.example.instagramfeedpreview.databinding.FragmentMainBinding
+import com.example.instagramfeedpreview.ui.component.instagram.InstagramFragment
 import com.example.instagramfeedpreview.ui.component.instagram.InstagramViewModel
+import com.example.instagramfeedpreview.ui.component.instagram.UiState
 import com.example.instagramfeedpreview.ui.component.main.adapter.FeedAdapter
 import com.example.instagramfeedpreview.ui.component.main.adapter.GridDividerItemDecoration
 import com.example.library.binding.BindingFragment
@@ -51,6 +55,25 @@ class MainFragment : BindingFragment<FragmentMainBinding>(R.layout.fragment_main
                 feedAdapter.submitList(boardDTO.data)
             }
         }
+
+        lifecycleScope.launchWhenCreated {
+            instagramViewModel.uiState.collectLatest {
+                when (it) {
+                    is UiState.Success -> {
+                        binding.progressBar.visibility = View.GONE
+                    }
+                    is UiState.Loading -> {
+                        binding.progressBar.visibility = View.VISIBLE
+                    }
+                    is UiState.Error -> {
+                        binding.progressBar.visibility = View.GONE
+                        Log.d(TAG, it.e.message.toString())
+                    }
+                    is UiState.Empty -> Unit
+                }
+            }
+        }
+
     }
     companion object {
         private const val TAG = "MainFragment"

c3a7c1ac4f8a32e5f7c9932406f0752c2f620611
YuYangWoo <yuyw0712@naver.com>
2023-10-03 14:47:17 +0900

    [#17] 안드로이드 멀티모듈 구성 - Login DTO로 네이밍 변경

--- data/dto/src/main/java/com/example/dto/LoginDTO.kt
+++ data/dto/src/main/java/com/example/dto/LoginDTO.kt
@@ -2,14 +2,13 @@ package com.example.dto
 
 import com.example.model.Login
 
-data class login(
+data class LoginDTO(
     val clientId: String,
     val clientSecret: String,
     val grantType: String,
     val redirectUri: String,
     val code: String
-) {
-    fun login.toDomain(): Login {
-        return Login(clientId, clientSecret, grantType, redirectUri, code)
-    }
+)
+fun LoginDTO.toDomain(): Login {
+    return Login(clientId, clientSecret, grantType, redirectUri, code)
 }

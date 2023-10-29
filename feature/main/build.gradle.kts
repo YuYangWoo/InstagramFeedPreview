dedc93de5c22c998485b527d8bf6361a990b7a45
YuYangWoo <yuyw0712@naver.com>
2023-10-29 22:50:28 +0900

    [#28] build-logic 구현 - HiltPlugin 구현

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -2,43 +2,10 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id ("kotlin-kapt")
-    id ("androidx.navigation.safeargs.kotlin")
-    id ("com.google.dagger.hilt.android")
-}
-
-android {
-    namespace = "com.example.main"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 14
-    }
-
-    testOptions {
-        unitTests.all {
-            it.useJUnitPlatform()
-        }
-    }
-
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-    }
-    buildFeatures {
-        viewBinding = true
-    }
+    id ("instagram.hilt")
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.android.material)
-    implementation(libs.androidx.constraintLayout)
-    implementation(libs.androidx.navigation.fragment)
-    implementation(libs.androidx.navigation.ktx)
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.compiler)
     implementation(project(":domain:usecase"))
 
     testImplementation(libs.bundles.kotest)

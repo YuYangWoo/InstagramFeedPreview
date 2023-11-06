0f6b775a2c7d25c5f0d990b09c4a30ee52d1622b
YuYangWoo <yuyw0712@naver.com>
2023-11-06 22:12:34 +0900

    [#28] build-logic 구현 - core:network 의존성 제거

--- data/repository/build.gradle.kts
+++ data/repository/build.gradle.kts
@@ -10,7 +10,6 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core)
 
     implementation(project(":core:datastore"))
-    implementation(project(":core:network"))
     implementation(project(":core:dto"))
     implementation(project(":domain:repository"))
     implementation(project(":domain:model"))

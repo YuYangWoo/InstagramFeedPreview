35ad943fa3b0f0770a8a9193f0ec440c0fe4c4d0
YuYangWoo <yuyw0712@naver.com>
2023-11-07 23:31:38 +0900

    [#28] build-logic 구현 - datastore gradle 정리

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -9,8 +9,5 @@ android {
 
 dependencies {
     implementation(libs.androidx.dataStore.preferences)
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.compiler)
-
     implementation(project(":data:datasource"))
 }

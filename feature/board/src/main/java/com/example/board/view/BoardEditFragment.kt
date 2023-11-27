4b477c6234411257f7c285496bdcc69c755584eb
YuYangWoo <yuyw0712@naver.com>
2023-11-28 02:07:30 +0900

    [#40] Room을 이용한 수정화면 만들기 - Room DB 데이터 삭제 기능 구현

--- feature/board/src/main/java/com/example/board/view/BoardEditFragment.kt
+++ feature/board/src/main/java/com/example/board/view/BoardEditFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
@@ -50,9 +51,13 @@ class BoardEditFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Toast.makeText(requireContext(), "편집모드", Toast.LENGTH_SHORT).show()
+
         initRecyclerView()
         initObserver()
         initRequest()
+        initClickListener()
     }
 
     private fun initRequest() {
@@ -80,9 +85,36 @@ class BoardEditFragment : BottomSheetDialogFragment() {
         }
     }
 
+    private fun initClickListener() {
+        binding.trashCanImageView.setOnClickListener {
+            Toast.makeText(requireContext(), "삭제할 사진을 선택해주세요.", Toast.LENGTH_SHORT).show()
+            binding.trashCanImageView.tag = if (binding.trashCanImageView.tag == true) {
+                binding.trashCanImageView.isSelected = false
+                false
+            } else {
+                binding.trashCanImageView.isSelected = true
+                true
+            }
+        }
+    }
+
     private fun initRecyclerView() {
-        binding.recyclerView.apply {
-            adapter = boardEditAdapter
+        with (binding.recyclerView) {
+            adapter = boardEditAdapter.apply {
+                setOnItemClickListener { board, position ->
+                    when (binding.trashCanImageView.tag) {
+                        true -> {
+                            boardViewModel.requestBoardItemDeleteAndSelect(board)
+                            boardEditAdapter.notifyItemRemoved(position)
+                        }
+                        else -> {
+                           // Nothing
+                        }
+                    }
+
+                }
+            }
+
             layoutManager = GridLayoutManager(context, 3)
 
             val callback = ItemMoveCallback(

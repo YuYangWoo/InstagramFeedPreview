f7794b2b09c582d2201970cd7b6bd2be6a06a4ff
YuYangWoo <yuyw0712@naver.com>
2023-12-07 01:54:52 +0900

    [#42] 각종 코드 리팩토링 건 - app di DatastoreSourceModule 네이밍 수정

--- app/src/main/java/com/example/instagramfeedpreview/di/DatastoreSourceModule.kt
+++ app/src/main/java/com/example/instagramfeedpreview/di/DatastoreSourceModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface DatastoreModule {
+interface DatastoreSourceModule {
     @Binds
     @Singleton
     fun provideDataStoreModule(userDataStoreSourceImpl: UserDataStoreSourceImpl): UserDataStoreSource

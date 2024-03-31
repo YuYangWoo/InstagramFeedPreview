283650c81aedb64313ac5b8442295f6ef21621fc
YuYangWoo <yuyw0712@naver.com>
2024-03-31 23:09:27 +0900

    [#49] layer 별 네이밍 변경 및 재설계 - FetchInstagramTokenUseCase 코드 리팩토링

--- domain/usecase/src/main/java/com/example/usecase/FetchInstagramTokenUseCase.kt
+++ domain/usecase/src/main/java/com/example/usecase/FetchInstagramTokenUseCase.kt
@@ -3,9 +3,9 @@ package com.example.usecase
 import com.example.model.LoginEntity
 import com.example.model.LongTokenEntity
 import com.example.repository.InstagramRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -13,17 +13,16 @@ import javax.inject.Singleton
 class FetchInstagramTokenUseCase @Inject constructor(
     private val instagramRepository: InstagramRepository
 ) {
+    @OptIn(ExperimentalCoroutinesApi::class)
     operator fun invoke(loginEntity: LoginEntity): Flow<LongTokenEntity> {
         return instagramRepository.fetchShortToken(loginEntity)
-            .map {
+            .flatMapLatest { shortToken ->
                 instagramRepository.fetchLongToken(
                     "ig_exchange_token",
                     loginEntity.clientSecret,
-                    it.accessToken,
+                    shortToken.accessToken,
                 )
             }
-            .flatMapLatest {
-                it
-            }
+
     }
 }

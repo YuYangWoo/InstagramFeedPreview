f8afd2cde048ea2e9e9e0e621cdcf65cb0f2dff8
YuYangWoo <yuyw0712@naver.com>
2024-08-27 02:17:09 +0900

    [#65] MainViewModel 코드 리팩토링

--- feature/main/src/main/java/com/example/main/MainViewModel.kt
+++ feature/main/src/main/java/com/example/main/MainViewModel.kt
@@ -5,23 +5,45 @@ import androidx.lifecycle.viewModelScope
 import com.example.usecase.FetchUserAccessTokenUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     fetchUserAccessTokenUseCase: FetchUserAccessTokenUseCase
 ) : ViewModel() {
-    val mainUiState = fetchUserAccessTokenUseCase().map {
-        if (it.isEmpty()) {
-            MainUiState(isLoading = false, isShowBoardFragment = false, accessToken = it)
+
+    val mainUiState = fetchUserAccessTokenUseCase()
+        .onStart {
+            MainUiState.Loading
+        }.map { accessToken ->
+            if (accessToken.isEmpty()) {
+                MainUiState.Success(isShowBoardFragment = false, accessToken = accessToken)
+            } else {
+                MainUiState.Success(isShowBoardFragment = true, accessToken = accessToken)
+            }
+        }.catch { throwable ->
+            onException(throwable)
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = MainUiState.Loading
+        )
+
+    private fun onException(throwable: Throwable): MainUiState.Error.ErrorState {
+        return if (throwable is IOException) {
+            MainUiState.Error.ErrorState.NetworkError(
+                "네트워크에 접속할 수 없습니다. 네트워크 연결상태 확인 후 다시 시도해 주세요.", throwable
+            )
         } else {
-            MainUiState(isLoading = false, isShowBoardFragment = true, accessToken = it)
+            MainUiState.Error.ErrorState.DefaultError(
+                "알 수 없는 오류입니다. 잠시 후 다시 시도해 주세요.", throwable
+            )
         }
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.Eagerly,
-        initialValue = MainUiState(isLoading = true, isShowBoardFragment = false)
-    )
+    }
+
 }
